17f421bfbf68c3eb155ea14fb6e049c8f3f171a2
Guuzzeji <5lh8dl440f@gmail.com>
2024-10-18 17:45:40 -0700

    adding eslint config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default {
+    rules: {
+        "arrow-spacing": "warn",
+        "brace-style": ["error", "stroustrup"],
+        "comma-spacing": "error",
+        "comma-style": "error",
+        "curly": ["error", "multi-line"],
+        "dot-location": ["error", "property"],
+        "indent": ["warn", "off"], // use 2 spaces for indentation
+        "keyword-spacing": "error",
+        "no-console": "off", // allow console logs for development
+        "no-empty-function": "error",
+        "no-trailing-spaces": "error",
+        "no-var": "error",
+        "semi": ["error", "always"],
+        "space-before-blocks": "error",
+        "space-infix-ops": "error",
+    },
+};
